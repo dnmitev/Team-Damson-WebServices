07ca6dfd66d8a7846524a5f6da2e03c1d292922d
susankelvin <susankelvin@users.noreply.github.com>
2014-09-18 18:07:13 +0300

    Default data transfer type set to JSON

--- BullsAndCows/BullsAndCows.Services/App_Start/WebApiConfig.cs
+++ BullsAndCows/BullsAndCows.Services/App_Start/WebApiConfig.cs
@@ -24,6 +24,10 @@
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional });
+
+
+            config.Formatters.JsonFormatter.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.None;
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
         }
     }
 }
